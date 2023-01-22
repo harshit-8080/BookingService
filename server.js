06b430aab12b983872bcfc40a3199c2da043af37
harshit-gyt <harshitraj@gytworkz.com>
2023-01-22 19:45:31 +0530

    adding routes for api gatewayadding routes for api gatewayadding routes for api gateway

--- server.js
+++ server.js
@@ -1,22 +1,18 @@
 const bodyParser = require("body-parser");
 const express = require("express");
-const {PORT} = require("./src/config/server.config.js");
+const { PORT } = require("./src/config/server.config.js");
 const app = express();
 const bookingRouter = require("./src/routes/booking.route");
 
-
 const setupAndStartServer = async (req, res) => {
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use("/booking", bookingRouter);
 
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended:true}));
-    app.use("/booking",bookingRouter);
-
-
-    app.listen(PORT, (req, res) => {
-        console.clear();
-        console.log(`Server started at ${PORT}`);
-    })
-
-}
+  app.listen(PORT, (req, res) => {
+    console.clear();
+    console.log(`Server started at ${PORT}`);
+  });
+};
 
 setupAndStartServer();
